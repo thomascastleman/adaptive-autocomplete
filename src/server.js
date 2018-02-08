cd0cbac1d7c25f3ca6e3a248b9a68031d2668c3b
Thomas Castleman <thomas7rex@gmail.com>
2018-02-07 21:14:53 -0500

    Debug

--- src/server.js
+++ src/server.js
@@ -33,3 +33,7 @@ server.listen(port, function() {
 
 
 // debug ---------------------------------------------------------
+var words = "Of on affixed civilly moments promise explain fertile in. Assurance advantage belonging happiness departure so of. Now improving and one sincerity intention allowance commanded not. Oh an am frankness be necessary earnestly advantage estimable extensive. Five he wife gone ye. Mrs suffering sportsmen earnestly any. In am do giving to afford parish settle easily garret.".split(" ");
+for (var i = 0; i < words.length; i++) {
+	stableTree.increment(words[i]);
+}
