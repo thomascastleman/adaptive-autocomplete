8fed7687ed6debd0b76fd57864bac661ca82d685
Thomas Castleman <thomas7rex@gmail.com>
2018-03-03 17:55:31 -0500

    Update db structure

--- src/create_db.sql
+++ src/create_db.sql
@@ -3,10 +3,32 @@ CREATE DATABASE adaptive_complete;
 
 USE adaptive_complete;
 
-CREATE TABLE test (
-	uid INT,
-	content TEXT
+-- stable tree backup
+CREATE TABLE stable_tree (
+	uid INT NOT NULL AUTO_INCREMENT,
+	data VARCHAR(1),
+	probability FLOAT,
+	uid_parent INT,
+	PRIMARY KEY (uid)
 );
 
+-- debug
+INSERT INTO stable_tree (uid, data, probability, uid_parent) VALUES (1, 'v', 2.4, 0);
 
-INSERT INTO test (content) VALUES ("Here is some test content");
+-- modifications table
+CREATE TABLE modifications (
+	word VARCHAR(32),
+	delta INT
+);
+
+-- debug
+INSERT INTO modifications (word, delta) VALUES ("testword", -2);
+
+
+CREATE TABLE novelty (
+	word VARCHAR(32),
+	user_frequency INT
+);
+
+-- debug
+INSERT INTO novelty (word, user_frequency) VALUES ("word", 50);
